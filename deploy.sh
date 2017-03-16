4410b1159d9b8b9e97f8c34224713828097e85eb
Vladislav Tomenko <you@example.com>
2017-03-16 12:47:53 +0700

    Debug

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e
 # authenticate to google cloud
 echo $GOOGLE_AUTH_JSON> key.json
 gcloud auth activate-service-account --key-file=key.json
-codeship_google authenticate
+#codeship_google authenticate
 
 # set compute zone
 gcloud config set compute/zone us-central1-a
